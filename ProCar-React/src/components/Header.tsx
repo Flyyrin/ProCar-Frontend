f37f0accc65188ad6d91e2d47991a467d193882c
Rafael Lemmen <BouncePlayR@gmail.com>
2024-09-16 16:25:12 +0200

    Created Header

---
+++ ProCar-React/src/components/Header.tsx
@@ -0,0 +1,110 @@
+import { NavLink } from "react-router-dom";
+import procarLogo from "/procar.png";
+import indicator from "../assets/indicator.svg";
+
+interface NotificationStatus {
+  messages?: boolean;
+  notifications?: boolean;
+}
+
+function Header({
+  notificationStatus = {},
+}: {
+  notificationStatus?: NotificationStatus;
+}) {
+  return (
+    <header>
+      <nav className="navbar navbar-expand navbar-light bg-white py-0 border-bottom border-2">
+        <div className="container-md navbar-container mx-auto">
+          <a className="navbar-brand" href="/">
+            <img
+              src={procarLogo}
+              alt=""
+              height={30}
+              className="d-inline-block align-middle me-1"
+            />
+            <span className="navbar-brand mb-0 h1">ProCar</span>
+          </a>
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item my-auto">
+                <NavLink
+                  className="nav-link my-0 text-dark underline"
+                  aria-current="page"
+                  to="/messages"
+                >
+                  <div className="d-flex align-items-center">
+                    <i className="bi bi-chat h5 mb-0 me-1 position-relative">
+                      {notificationStatus.messages && (
+                        <img
+                          src={indicator}
+                          alt="indicator"
+                          className="img-fluid position-absolute indicator text-danger"
+                        />
+                      )}
+                    </i>
+                    <span className="align-middle d-none d-md-flex">
+                      Berichten
+                    </span>
+                  </div>
+                </NavLink>
+              </li>
+              <li className="nav-item my-auto">
+                <NavLink
+                  className="nav-link my-0 text-dark underline"
+                  aria-current="page"
+                  to="/notifications"
+                >
+                  <div className="d-flex align-items-center">
+                    <i className="bi bi-bell h5 mb-0 me-1 position-relative">
+                      {notificationStatus.notifications && (
+                        <img
+                          src={indicator}
+                          alt="indicator"
+                          className="img-fluid position-absolute indicator text-danger"
+                        />
+                      )}
+                    </i>
+                    <span className="align-middle d-none d-md-flex">
+                      Meldingen
+                    </span>
+                  </div>
+                </NavLink>
+              </li>
+              <li className="nav-item my-auto">
+                <NavLink
+                  className="nav-link my-0 text-dark underline"
+                  aria-current="page"
+                  to="/account"
+                >
+                  <div className="d-flex align-items-center">
+                    <i className="bi bi-person h5 mb-0 me-1"></i>
+                    <span className="align-middle d-none d-md-flex">
+                      Account
+                    </span>
+                  </div>
+                </NavLink>
+              </li>
+              <li className="nav-item my-auto">
+                <a
+                  className="nav-link my-0 py-0"
+                  aria-current="page"
+                  href="sell"
+                >
+                  <div className="d-flex align-items-center btn btn-sm text-white mb-xs-2">
+                    <i className="bi bi-car-front h5 mb-0"></i>
+                    <span className="align-middle ms-2 d-none d-md-flex">
+                      Verkoop een auto
+                    </span>
+                  </div>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
